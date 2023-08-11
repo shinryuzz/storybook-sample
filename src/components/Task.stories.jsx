47e229ab9f17d05f8e045fd2091611b8cf9a12ab
Shinryuzz <mshin1127@gmail.com>
2023-08-11 23:38:32 +0900

    controls addon

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -16,6 +16,17 @@ export const Default = {
   },
 };
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
+    },
+  },
+};
+
 export const Pinned = {
   args: {
     task: {
